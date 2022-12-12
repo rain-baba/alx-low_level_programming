f89aaeacea6ee0128e7ca40aed61e826c7577be6
rain-baba <segenet248@gmail.com>
2022-12-12 05:49:11 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,40 +2,24 @@
 
 /**
  *
- *  * main - Prints the alphabet except the letters q and e.
+ * main - Prints the alphabet except the letters q and e.
  *
- *  (*
+ * Return: 0 on success
  *
- *   * Return: 0 on success
- *
- *    */
+ */
 
 int main(void)
-
 {
+	char c = 'a';
 
-		char c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')		
+		{ 												                 	putchar(c);
 
-
-
-			while (c <= 'z')
-
-					{
-
-								if (c != 'q' && c != 'e')
-
-											{
-
-															putchar(c);
-
-																	}
-
-										c++;
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
