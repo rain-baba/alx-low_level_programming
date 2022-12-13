a85f1e7fae023f90c843894b639e43236badb689
rain-baba <segenet248@gmail.com>
2022-12-13 10:05:34 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,54 +1,95 @@
 #include "main.h"
 
 /**
- * times_table -prints tables
  *
- * Return: Always 0.
+ *  * times_table -prints tables
  *
- */
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
 void times_table(void)
-{
-	int a;
-	int b;
-	int c;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar('0');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
-			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
-		}
-		_putchar ('\n');
-	}
-}
 
-		
-				
-			
+{
+
+		int i, j, n, c;
+
+
+
+			for (i = 0; i <= 9; i++)
+
+					{
+
+								c = 0;
+
+										for (j = 0; j <= 9; j++)
+
+													{
+
+																	n = i * j;
+
+																				if (n <= 9 && i * (j + 1) <= 9)
+
+																								{
+
+																													_putchar(n + '0');
+
+																																	if (c != 9)
+
+																																						{
+
+																																												_putchar(',');
+
+																																																	_putchar(' ');
+
+																																																						_putchar(' ');
+
+																																																										}
+
+																																				}
+
+																							else
+
+																											{
+
+																																if (n / 10 == 0)
+
+																																					{
+
+																																											_putchar(n + '0');
+
+																																															}
+
+																																				else
+
+																																									{
+
+																																															_putchar((n / 10) + '0');
+
+																																																				_putchar((n % 10) + '0');
+
+																																																								}
+
+																																								if (c != 9)
+
+																																													{
+
+																																																			_putchar(',');
+
+																																																								_putchar(' ');
+
+																																																												}
+
+																																											}
+
+																										c++;
+
+																												}
+
+												_putchar('\n');
+
+													}
+
+}
