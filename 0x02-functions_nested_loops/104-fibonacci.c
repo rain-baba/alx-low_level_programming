a4f8e8d5110668a82b16e27c2cd74ddd8b92a8c3
rain-baba <segenet248@gmail.com>
2022-12-14 01:16:50 -0800

    My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,39 +11,22 @@
 
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	float first, second, next;
+	int c;
 
-	j = 1;
-	k = 2;
+	first = 1;
+	second = 2;
+	c = 2;
 
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	printf("%.f, %.f", first, second);
+	while (c  < 98)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		next = first + second;
+		first = second;
+		second = next;
+		printf(", %.f", next);
+		c++;
 	}
-
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;	
-		k2 = k2 + j2;	
-		j2 = k2 - j2;
-	}
-
 	printf("\n");
-
 	return (0);
 }
-			
-									}
-
