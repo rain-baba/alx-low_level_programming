92c83f29aa4cb4b78ee8fa9505f5cc2c8961e331
rain-baba <segenet248@gmail.com>
2022-12-16 03:31:55 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -15,11 +16,11 @@ int main(void)
 	/**
 	 * while (i < 10)
 	 * {
-	 * 	putchar(i);
+	 * putchar(i);
 	 * }
 	 */
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
-
+	
