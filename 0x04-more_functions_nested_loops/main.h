fbe793270fcdbbc68510314f4a7001acf25ea576
rain-baba <segenet248@gmail.com>
2022-12-16 04:49:54 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * main - header file for protoypes
+ * void protoypes (void
+ * int protoypes (int)
+ */
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
