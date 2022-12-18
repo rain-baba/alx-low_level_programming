a8a98c3c07106658799f6136d5755589ef7b519f
rain-baba <segenet248@gmail.com>
2022-12-18 00:49:47 -0800

    I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-/**
- * main - prints the alphabet in lowercase.
- *
- * Return: 0 on success
- *
- */
-int main(void)
- {
-		char c = 'a';
-		while (c <= 'z')
-		{
-			putchar(c);
-			c++;
-		}
-		putchar('\n');
-		return (0);
-}
+#include <ctype.h>
 
+/**
+ * main - Program to print alphabet letter followed by new line
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
